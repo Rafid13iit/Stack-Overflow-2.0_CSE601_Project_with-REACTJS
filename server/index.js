cd3b885b557293e84551a05d3ccb111418f580fb
Rafid13iit <rafidssrr.6767@gmail.com>
2024-10-24 15:10:38 +0600

    Auth Problem Resolved

--- server/index.js
+++ server/index.js
@@ -22,9 +22,10 @@ app.use('/questions', questionRoutes )
 app.use('/answer', answerRoutes)
 app.use('/comment', commentRoutes)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
 const DATABASE_URL = process.env.CONNECTION_URL
+// console.log('Database URL:', DATABASE_URL);
 
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
         .then(() => app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)}))
